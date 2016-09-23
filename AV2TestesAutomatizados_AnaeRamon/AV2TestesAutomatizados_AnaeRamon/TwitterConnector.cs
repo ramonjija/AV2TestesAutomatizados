72c6b6b6feaf17d8961dbf0de7bf4bc72d327477
jacobsenanaizabel <ana.izabel@vtex.com.br>
2016-09-23 03:06:59 -0300

    classe de autorizacao para o twitter bot

---
+++ AV2TestesAutomatizados_AnaeRamon/AV2TestesAutomatizados_AnaeRamon/TwitterConnector.cs
@@ -0,0 +1,19 @@
+﻿using LinqToTwitter;
+
+namespace AV2TestesAutomatizados_AnaeRamon
+{
+    public class TwitterConnector
+    {
+        private SingleUserAuthorizer authorizer = new SingleUserAuthorizer
+        {
+            CredentialStore = new SingleUserInMemoryCredentialStore
+            {
+                ConsumerKey = "nYHCaQAkZ695OUiiN52E6UHWa",
+                ConsumerSecret = "wkBV85KVAR8ecbI7Z5i5YsoVbgegaGUnv26eBichhbj86C703h",
+                AccessToken =  "775706440128487424-q2Wp7EUFIMBxX8li9CzdSqpmnRyPmZ9",
+                AccessTokenSecret = "yWrfJ2lvZuUIeMpBRefhnawN7CFUvlK6y3kDCVx8iHK1b"
+            }
+        };
+
+    }
+}
